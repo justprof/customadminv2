df63d2e1692f8072bc9894e1ba42313384ef9435
justprof <prof5432@hotmail.com>
2025-03-08 20:48:28 +0300

    Add Chakra UI react-router-dom and sidebar navigation

---
+++ customadmin/src/components/LinkItems.jsx
@@ -0,0 +1,18 @@
+import {
+    FiHome,
+    FiTrendingUp,
+    FiCompass,
+    FiStar,
+    FiSettings,
+  } from "react-icons/fi"; 
+
+  const LinkItems = [
+    
+  { name: "Home", icon: FiHome, path: "/" },
+   { name: "Trending", icon: FiTrendingUp, path: "/trending" },
+   { name: "Explore", icon: FiCompass, path: "/explore" },
+   { name: "Favourites", icon: FiStar, path: "/favourites" },
+   { name: "Settings", icon: FiSettings, path: "/settings" },
+ ];
+ 
+ export default LinkItems;
---
+++ customadmin/src/components/NavItem.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+ import { Box, Flex, Icon } from "@chakra-ui/react";
+ import { Link } from "react-router-dom";
+ const NavItem = ({ icon, children, path, ...rest }) => {
+  return (
+    <Box 
+    as={Link}
+       to={path}
+       style={{ textDecoration: "none" }}
+       _focus={{ boxShadow: "none" }}
+     >
+       <Flex
+         align="center"
+         p="4"
+         mx="4"
+         borderRadius="lg"
+         role="group"
+         cursor="pointer"
+         _hover={{
+           bg: "cyan.400",
+           color: "white",
+         }}
+         {...rest}
+       >
+         {icon && (
+           <Icon
+             mr="4"
+             fontSize="16"
+             _groupHover={{
+               color: "white",
+             }}
+             as={icon}
+             />
+         )}
+         {children}
+       </Flex>
+     </Box>
+   );
+ };
+ 
+ export default NavItem;
