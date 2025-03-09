ddd7bf14ed3850d6b36c64642f1bac1b84292547
justprof <prof5432@hotmail.com>
2025-03-09 05:50:49 +0300

    Refactor Sidebar component update dependencies and Update  AdminLayout and AdminRoutes

--- customadmin/src/App.jsx
+++ customadmin/src/App.jsx
@@ -1,11 +1,18 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+ import AdminLayout from "./layouts/AdminLayout";
+ import Home from "./pages/home";
 
 function App() {
  
-  return <>app</>;
+  return (
+    <BrowserRouter>
+    <Routes>
+      <Route path="/admin/*" element ={<AdminLayout />}  /> 
+      <Route path="/" element = {<Home/>} />
+      </Routes>
+      </BrowserRouter>
+  )
    
 }
 
---
+++ customadmin/src/layouts/AdminLayout.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Outlet } from "react-router-dom";
+import SidebarWithHeader from "../components/sidebar";
+import AdminRoutes from "../routes/AdminRoutes";
+import Dashboard from "../admin/pages/dashboard";
+ 
+ const AdminLayout = () => {
+   return (
+     <div>
+       <SidebarWithHeader />
+       <AdminRoutes />
+       <Outlet />
+     </div>
+   );
+ };
+ 
+ export default AdminLayout;
