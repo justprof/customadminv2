1ed867c4af03053ff96aded635d8b8deb44aa4b5
justprof <prof5432@hotmail.com>
2025-03-12 07:12:05 +0300

    add auth / login and signup

--- customadmin/src/App.jsx
+++ customadmin/src/App.jsx
@@ -1,7 +1,8 @@
-import { useState } from 'react'
 import { BrowserRouter, Route, Routes } from "react-router-dom";
- import AdminLayout from "./layouts/AdminLayout";
- import Home from "./pages/home";
+import AdminLayout from "./layouts/AdminLayout";
+import Home from "./pages/home";
+import Login from "./pages/auth/login";
+import Signup from "/.pages/auth/signup";
 
 function App() {
  
@@ -10,10 +11,12 @@ function App() {
     <Routes>
       <Route path="/admin/*" element ={<AdminLayout />}  /> 
       <Route path="/" element = {<Home/>} />
+      <Route path="/login" element ={<Login />} />
+      <Route path="/signup" element = {<Signup />} />
       </Routes>
       </BrowserRouter>
   )
    
 }
 
-export default App
+export default App;
