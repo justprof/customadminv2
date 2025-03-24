d521ab43d70a384195ddf2c17c2f3ca0e43a774e
justprof <prof5432@hotmail.com>
2025-03-24 06:04:57 +0300

    feat: add Chakra UI v3 system-based custom theme compatible with React 19

--- customadmin/src/main.jsx
+++ customadmin/src/main.jsx
@@ -4,13 +4,27 @@ import App from "./App.jsx";
 import "./index.css";
 import { Provider } from "react-redux";
 import store from "./store/store.js";
-import { ChakraProvider } from "@chakra-ui/react";
+import { createSystem, defaultConfig, ChakraProvider } from "@chakra-ui/react";
 
+// Chakra v3 uyumlu tema
+const system = createSystem(defaultConfig, {
+  theme: {
+    tokens: {
+      colors: {
+        brand: {
+          900: { value: "#1a365d" },
+          800: { value: "#153e75" },
+          700: { value: "#2a69ac" },
+        },
+      },
+    },
+  },
+});
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
      <Provider store={store}>
-       <ChakraProvider theme={theme}>
+       <ChakraProvider value={system}>
           <App />
        </ChakraProvider>
      </Provider>
