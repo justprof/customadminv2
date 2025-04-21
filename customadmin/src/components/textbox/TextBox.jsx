870a709e60fdc8239c79f800314fb4162e2a6440
justprof <prof5432@hotmail.com>
2025-04-21 04:42:58 +0300

    fix chakra ui v3 datatable and form

--- customadmin/src/components/textbox/TextBox.jsx
+++ customadmin/src/components/textbox/TextBox.jsx
@@ -1,48 +1,43 @@
 import React, { useState, useEffect } from "react";
- import {
-   FormControl,
-   FormLabel,
-   Input,
-   FormErrorMessage,
- } from "@chakra-ui/react";
- 
- const TextBox = ({
-   name,
-   label,
-   placeholder,
-   initialValue = "",
-   getFinalValue,
-   isRequired = false,
-   error = "",
-   type = "text",
-   ...props
- }) => {
-   const [value, setValue] = useState(initialValue);
- 
-   const handleChange = (e) => {
-     setValue(e.target.value);
-   };
- 
-   useEffect(() => {
-     if (getFinalValue) {
-       getFinalValue(value);
-     }
-   }, [value, getFinalValue]);
- 
-   return (
-     <FormControl isRequired={isRequired} isInvalid={error}>
-       {label && <FormLabel>{label}</FormLabel>}
-       <Input
-         type={type}
-         name={name}
-         placeholder={placeholder}
-         value={value}
-         onChange={handleChange}
-         {...props}
-       />
-       {error && <FormErrorMessage>{error}</FormErrorMessage>}
-     </FormControl>
-   );
- };
- 
- export default TextBox;
+import { Field, Input } from "@chakra-ui/react";
+
+const TextBox = ({
+  name,
+  label,
+  placeholder,
+  initialValue = "",
+  getFinalValue,
+  isRequired = false,
+  error = "",
+  type = "text",
+  ...props
+}) => {
+  const [value, setValue] = useState(initialValue);
+
+  const handleChange = (e) => {
+    setValue(e.target.value);
+  };
+
+  useEffect(() => {
+    if (getFinalValue) {
+      getFinalValue(value);
+    }
+  }, [value, getFinalValue]);
+
+  return (
+    <Field.Root required={isRequired} invalid={!!error}>
+      <Field.Label>{label}</Field.Label>
+      <Input
+        type={type}
+        name={name}
+        placeholder={placeholder}
+        value={value}
+        onChange={handleChange}
+        {...props}
+      />
+      {error && <Field.ErrorText>{error}</Field.ErrorText>}
+    </Field.Root>
+  );
+};
+
+export default TextBox;
